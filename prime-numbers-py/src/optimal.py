1b288fa45d678151e03521d2c764d8b9f2210683
Manuel Sandoval <manusandoval05@gmail.com>
2021-04-13 11:35:02 -0500

    Initial preparations for multithreading

--- prime-numbers-py/src/optimal.py
+++ prime-numbers-py/src/optimal.py
@@ -1,28 +1,30 @@
 import numpy as np
-from numba import njit, vectorize
+from numba import njit
 
 digits = int(input("Number to calculate: "))
 threads = int(input("Number of threads in system: "))
-prime_numbers = np.ones(digits)
-array_length = len(prime_numbers)
+prime_numbers_array = np.ones(digits)
+array_length = len(prime_numbers_array)
 factor = 3
 
+print(prime_numbers_array)
+
 @njit 
-def calculate_prime_numbers(factor, length, array):
+def calculate_prime_numbers(factor, array):
   while factor < array_length:
     print(factor)
     index = factor
     while index < array_length:
       index += factor
-      if index <= length:
+      if index <= array_length:
         if array[index - 2] == True:
           array[index - 2] = False
     factor += 2
     if array[factor - 2] == False:
       while array[factor - 2] == False:
         factor += 2
-        if factor > length:
+        if factor > array_length:
           break
   return array
 
-primes = calculate_prime_numbers(factor=factor, length=array_length, array=prime_numbers)
+primes = calculate_prime_numbers(factor=factor, array= prime_numbers_array)
